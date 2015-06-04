a7fa517bd81e0843357b4fb8328e5fa1ff8c5f2d
Chris Marques <test.code.debug@gmail.com>
2015-06-04 15:31:08 -0400

    Add ability to add and delete contacts to ContactList

--- lib/chiketto/contact_list.rb
+++ lib/chiketto/contact_list.rb
@@ -10,6 +10,18 @@ module Chiketto
       contacts.map { |c| Contact.new c }
     end
 
+    def add_contact(params)
+      raise Exception.new("ContactList must have an id and user_id to add a Contact") unless id && user_id
+      response = self.class.post("users/#{user_id}/contact_lists/#{id}/contacts", params)
+      response["created"]
+    end
+
+    def delete_contact(email)
+      raise Exception.new("ContactList must have an id and user_id to delete its Contacts") unless id && user_id
+      response = self.class.delete("users/#{user_id}/contact_lists/#{id}/contacts", { email: email })
+      response["deleted"]
+    end
+
     private
 
     def self.find_contacts(id, params)
--- test/contact_list_test.rb
+++ test/contact_list_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class ContactListTest < MiniTest::Test
-  USER_ID = 72013652427
-  CONTACT_LIST_ID = 411987
+  USER_ID = 56727857661
+  CONTACT_LIST_ID = 859438
 
   def test_contact_list_exposes_data
     contact_list = Chiketto::ContactList.new
@@ -20,6 +20,35 @@ class ContactListTest < MiniTest::Test
     contact_list = Chiketto::ContactList.new({ id: CONTACT_LIST_ID })
     assert_raises Chiketto::Exception do
       contact_list.contacts
+    end
+  end
+
+  def test_contact_list_add_contact_returns_true
+    contact_list = Chiketto::ContactList.new({ id: CONTACT_LIST_ID, user_id: USER_ID })
+    params = { "contact.email" => "foo@bar.com", "contact.first_name" => "foo", "contact.last_name" => "bar" }
+    VCR.use_cassette 'contact-list-add-contact' do
+      assert_kind_of TrueClass, contact_list.add_contact(params)
+    end
+  end
+
+  def test_contact_list_without_user_id_add_contact_raises_error
+    contact_list = Chiketto::ContactList.new({ id: CONTACT_LIST_ID })
+    assert_raises Chiketto::Exception do
+      contact_list.add_contact({})
+    end
+  end
+
+  def test_contact_list_delete_contact_returns_true
+    contact_list = Chiketto::ContactList.new({ id: CONTACT_LIST_ID, user_id: USER_ID })
+    VCR.use_cassette 'contact-list-delete-contact' do
+      assert_kind_of TrueClass, contact_list.delete_contact("foo@bar.com")
+    end
+  end
+
+  def test_contact_list_without_user_id_delete_contact_raises_error
+    contact_list = Chiketto::ContactList.new({ id: CONTACT_LIST_ID })
+    assert_raises Chiketto::Exception do
+      contact_list.delete_contact("")
     end
   end
 end
