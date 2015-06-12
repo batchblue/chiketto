45ba2afc7bc502e4e61182d92554d388e9f61b44
Chris Marques <test.code.debug@gmail.com>
2015-06-12 11:12:15 -0400

    Convert User tests from VCR to Webmock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,18 @@
 require 'chiketto'
 require 'dotenv'
 require 'minitest/autorun'
-require 'vcr'
+require 'webmock/minitest'
 
 Dotenv.load
 
+require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'test/vcr'
   c.hook_into :webmock
 end
+
+def stub_api_calls?
+  ENV['LIVE_API_CALLS'] != "true"
+end
+
+require 'mocks/user_mocks'
--- test/user_test.rb
+++ test/user_test.rb
@@ -1,13 +1,10 @@
 require 'test_helper'
 
 class UserTest < MiniTest::Test
-  USER_ID = 72013652427
-  CONTACT_LIST_ID = 411987
+  def setup
+    stub_users_me
 
-  def find_me
-    VCR.use_cassette 'user-me' do
-      @user = Chiketto::User.find
-    end
+    @user = Chiketto::User.find
   end
 
   def test_exposes_data
@@ -20,77 +17,57 @@ class UserTest < MiniTest::Test
   end
 
   def test_find_without_id
-    find_me
     assert_kind_of Chiketto::User, @user
   end
 
   def test_find_with_id
-    VCR.use_cassette 'user-id' do
-      @user = Chiketto::User.find USER_ID
-    end
+    stub_users_id
+
+    @user = Chiketto::User.find USER_ID
     assert_kind_of Chiketto::User, @user
   end
 
   def test_user_has_emails
-    find_me
     assert_kind_of Array, @user.emails
     assert_kind_of Chiketto::Email, @user.emails.first
   end
 
-  def test_user_has_email_for_primary_only
-    find_me
-    assert_kind_of Chiketto::Email, @user.email
-  end
-
   def test_user_has_events
-    find_me
+    stub_user_events
 
-    VCR.use_cassette 'user-events' do
-      assert_kind_of Chiketto::Event, @user.events.first
-    end
+    assert_kind_of Chiketto::Event, @user.events.first
   end
 
   def test_user_has_event_attendees
-    find_me
+    stub_user_attendees
 
-    VCR.use_cassette 'user-event-attendees' do
-      assert_kind_of Chiketto::Attendee, @user.event_attendees.first
-    end
+    assert_kind_of Chiketto::Attendee, @user.event_attendees.first
   end
 
   def test_user_has_organizers
-    find_me
+    stub_user_organizers
 
-    VCR.use_cassette 'user-organizers' do
-      assert_kind_of Chiketto::Organizer, @user.organizers.first
-    end
+    assert_kind_of Chiketto::Organizer, @user.organizers.first
   end
 
   def test_user_has_contact_lists
-    find_me
+    stub_user_contact_lists
 
-    VCR.use_cassette 'user-contact-lists' do
-      assert_kind_of Chiketto::ContactList, @user.contact_lists.first
-    end
+    assert_kind_of Chiketto::ContactList, @user.contact_lists.first
   end
 
   def test_user_find_contact_list_returns_contact_list
-    find_me
+    stub_user_contact_list
 
-    VCR.use_cassette 'user-find-contact-list' do
-      assert_kind_of Chiketto::ContactList, @user.find_contact_list(CONTACT_LIST_ID)
-    end
+    assert_kind_of Chiketto::ContactList, @user.find_contact_list(CONTACT_LIST_ID)
   end
 
   def test_create_contact_list
-    find_me
+    stub_user_create_contact_list
 
-    VCR.use_cassette 'user-create-contact-list' do
-      contact_list = @user.create_contact_list({
-        'contact_list.name' => 'Test Contact List Creation'
-      })
-      assert_kind_of Chiketto::ContactList, contact_list
-      assert_equal 'Test Contact List Creation', contact_list.name
-    end
+    contact_list = @user.create_contact_list({
+      'contact_list.name' => 'Test Contact List Creation'
+    })
+    assert_kind_of Chiketto::ContactList, contact_list
   end
 end
